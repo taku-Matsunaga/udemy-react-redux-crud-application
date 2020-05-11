a3564cf21313cc028eac47ad9b315560e5654b42
taku-Matsunaga <skyline199306@yahoo.co.jp>
2020-05-11 13:39:36 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class App extends Component{
   render(){
     return(
-      <div><h1>Hello, World!</h1></div>
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("cicked!")}} />
+      </React.Fragment>
     )
   }
 }
